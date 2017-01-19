cd24ad1ee3f08c773cb64d4e4091569f4662fbc0
Joe F. Student <huangweipeng@172.25.136.238>
2017-01-19 18:28:16 -0500

    hello world

---
+++ src/HelloWorld/main.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("Hello world!");
+           
+           
+	}
+
+}
